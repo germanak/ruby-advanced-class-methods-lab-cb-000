634255be326a315f67885fd1f7e61c1039a82e83
Alec German <alec_german@yahoo.com>
2018-02-13 21:48:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,13 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    self.create
+  end
 end
